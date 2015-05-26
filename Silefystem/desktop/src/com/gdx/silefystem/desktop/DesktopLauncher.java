d566162bbfb98dc5b812b5fde60e8e2bfe67d52f
Syrose <Syrose96@gmail.com>
2015-05-26 22:57:08 +0300

    Core
    Added core

---
+++ Silefystem/desktop/src/com/gdx/silefystem/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.silefystem.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.silefystem.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
